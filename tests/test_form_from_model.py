45a80c9264d6d7ababdfb647128172d6ac19d779
Evren Esat Özkan <sleytr@gmail.com>
2015-08-31 07:45:14 +0300

    re-added  model>form serialization

--- tests/test_form_from_model.py
+++ tests/test_form_from_model.py
@@ -1,30 +1,24 @@
 # -*-  coding: utf-8 -*-
 
-from tests.deep_eq import deep_eq
-# from tests.models import Employee
+# from tests.deep_eq import deep_eq
+from zengine.models import User
 from zengine.lib.forms import JsonForm
 
-__author__ = 'Evren Esat Ozkan'
-
-serialized_empty_test_employee = {
-    'model': {'birth_date': '', 'last_name': None,
-              'first_name': None, 'staff_type': None},
-    'form': ['first_name', 'last_name', 'staff_type', 'birth_date'],
+serialized_empty_user = {
+    'model': {'username': None, 'password': None},
+    'form': ['username', 'password'],
     'schema': {
-        'required': ['first_name', 'staff_type', 'birth_date', 'last_name'],
+        'required': ['username', 'password'],
         'type': 'object',
         'properties': {
-            'birth_date': {'type': 'date', 'title': 'Doğum Tarihi'},
-            'last_name': {'type': 'string', 'title': 'Soyadı'},
-            'first_name': {'type': 'string', 'title': 'Adı'},
-            'staff_type': {'type': 'string',
-                           'title': 'Personel Türü'}},
-        'title': 'Employee'}}
+            'username': {'type': 'string', 'title': 'Username'},
+            'password': {'type': 'password', 'title': 'Password'},
+        },
+        'title': 'User'}}
 
-#
-# def test_simple():
-#     serialized_form = JsonForm(Employee()).serialize()
-#     # assert serialized_empty_test_employee['model'] == serialized_form['model']
-#     assert deep_eq(serialized_empty_test_employee, serialized_form,
-#                    _assert=True)
 
+def test_simple_serialize():
+    serialized_form = JsonForm(User(), types={"password": "password"}).serialize()
+    # assert serialized_empty_test_employee['model'] == serialized_form['model']
+    assert serialized_empty_user == serialized_form
+    # assert deep_eq(serialized_empty_user, serialized_form, _assert=True)
