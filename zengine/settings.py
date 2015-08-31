c4cb94aa03d41c2e834baca13c1cebdbffb927e4
Evren Esat Özkan <evrenesat@zetaops.io>
2015-08-31 17:20:37 +0300

    converted WORKFLOW_PACKAGES_PATH to list

--- zengine/settings.py
+++ zengine/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 # path of the activity modules which will be invoked by workflow tasks
 ACTIVITY_MODULES_IMPORT_PATHS = ['zengine.activities']
 # absolute path to the workflow packages
-WORKFLOW_PACKAGES_PATH = os.path.join(BASE_DIR, 'workflows')
+WORKFLOW_PACKAGES_PATHS = [os.path.join(BASE_DIR, 'workflows')]
 
 AUTH_BACKEND = 'zengine.auth_backend.AuthBackend'
 
