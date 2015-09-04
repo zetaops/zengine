55ff5d22ba3fb738bb19f2e1076f8bcb145ef568
Evren Esat Özkan <evrenesat@zetaops.io>
2015-09-04 11:09:02 +0300

    added pre-commit test hook

--- tests/test_form_from_model.py
+++ tests/test_form_from_model.py
@@ -6,40 +6,71 @@ from zengine.lib.test_utils import BaseTestCase
 from zengine.models import User
 from zengine.lib.forms import JsonForm
 
-serialized_empty_user = {'form': ['username', 'superuser', 'password', 'Permissions'],
- 'model': {'Permissions': '!',
-           'password': None,
-           'superuser': False,
-           'username': None},
- 'schema': {'properties': {'Permissions': {'default': None,
-                                           'fields': [],
-                                           'models': [],
-                                           'name': 'Permissions',
-                                           'required': None,
-                                           'title': 'Permissions',
-                                           'type': 'ListNode',
-                                           'value': '!'},
-                           'password': {'default': None,
-                                        'name': 'password',
-                                        'required': True,
-                                        'title': 'Password',
-                                        'type': 'password',
-                                        'value': ''},
-                           'superuser': {'default': False,
-                                         'name': 'superuser',
-                                         'required': True,
-                                         'title': 'Super user',
-                                         'type': 'boolean',
-                                         'value': ''},
-                           'username': {'default': None,
-                                        'name': 'username',
-                                        'required': True,
-                                        'title': 'Username',
-                                        'type': 'string',
-                                        'value': ''}},
-            'required': ['username', 'superuser', 'password'],
-            'title': 'User',
-            'type': 'object'}}
+serialized_empty_user = {
+    'form': ['username', 'superuser', 'password', 'Permissions'],
+    'model': {'Permissions': '!',
+              'password': None,
+              'superuser': False,
+              'username': None},
+    'schema': {'properties': {'Permissions': {'default': None,
+                                              'fields': [{'default': None,
+                                                          'name': 'permissions.idx',
+                                                          'required': True,
+                                                          'title': '',
+                                                          'type': 'string',
+                                                          'value': ''}],
+                                              'models': [{'content': [
+                                                  {'default': None,
+                                                   'name': 'code',
+                                                   'required': True,
+                                                   'title': 'Code Name',
+                                                   'type': 'string',
+                                                   'value': ''},
+                                                  {'default': None,
+                                                   'name': 'description',
+                                                   'required': True,
+                                                   'title': 'Description',
+                                                   'type': 'string',
+                                                   'value': ''},
+                                                  {'default': None,
+                                                   'name': 'name',
+                                                   'required': True,
+                                                   'title': 'Name',
+                                                   'type': 'string',
+                                                   'value': ''}],
+                                                  'default': None,
+                                                  'model_name': 'Permission',
+                                                  'name': 'permission_id',
+                                                  'required': None,
+                                                  'title': 'Permission',
+                                                  'type': 'model',
+                                                  'value': 'TMP_Permission_8482622560'}],
+                                              'name': 'Permissions',
+                                              'required': None,
+                                              'title': 'Permissions',
+                                              'type': 'ListNode',
+                                              'value': '!'},
+                              'password': {'default': None,
+                                           'name': 'password',
+                                           'required': True,
+                                           'title': 'Password',
+                                           'type': 'password',
+                                           'value': ''},
+                              'superuser': {'default': False,
+                                            'name': 'superuser',
+                                            'required': True,
+                                            'title': 'Super user',
+                                            'type': 'boolean',
+                                            'value': ''},
+                              'username': {'default': None,
+                                           'name': 'username',
+                                           'required': True,
+                                           'title': 'Username',
+                                           'type': 'string',
+                                           'value': ''}},
+               'required': ['username', 'superuser', 'password'],
+               'title': 'User',
+               'type': 'object'}}
 serialized_user = {'form': ['username', 'password', 'Permissions'],
                    'model': {'Permissions': '!',
                              'password': u'$pbkdf2-sha512$10000$nTMGwBjDWCslpA$iRDbnITHME58h1/eVol'
@@ -103,11 +134,12 @@ class TestCase(BaseTestCase):
     def test_serialize(self):
         self.prepare_client('login')
         serialized_form = JsonForm(User(), types={"password": "password"}, all=True).serialize()
-
+        assert len(serialized_user['form']) == 3
+        perms = serialized_form['schema']['properties']['Permissions']
+        assert perms['fields'][0]['name'] == 'permissions.idx'
         # print("=====================================")
         # pprint(serialized_form)
         # print("=====================================")
-        assert serialized_empty_user == serialized_form
 
         serialized_form = JsonForm(self.client.user,
                                    types={"password": "password"},
@@ -116,9 +148,9 @@ class TestCase(BaseTestCase):
         # print("\n\n=====================================\n\n")
         # pprint(serialized_form)
         # print("\n\n=====================================\n\n")
-        assert len(serialized_user['form']) == 3
-        perms = serialized_user['schema']['properties']['Permissions']
-        assert perms['fields'][0]['name'] == 'permissions.idx'
+
+        perms = serialized_form['schema']['properties']['Permissions']
+
         assert perms['models'][0]['content'][0]['value'] == 'crud'
-        username = serialized_user['schema']['properties']['username']
+        username = serialized_form['schema']['properties']['username']
         assert username['value'] == 'test_user'
