606f5e18145bc9a08ef043e364f14b73e84e25e6
Evren Esat Özkan <evrenesat@zetaops.io>
2015-09-04 17:14:52 +0300

    added user permission management command

--- zengine/management_commands.py
+++ zengine/management_commands.py
@@ -51,3 +51,40 @@ class CreateUser(Command):
         user.set_password(self.manager.args.password)
         user.save()
         return "New user created with ID: %s" % user.key
+
+
+class SetPermission(Command):
+    CMD_NAME = 'set_perm'
+    HELP = "Gives permissions to a user. Only works for ZEngine's own User & Permission models"
+    PARAMS = [
+        ('username', True,
+         'Login username. Will list existing perms of the user if no other option given.'),
+        ('perms', False, 'Permission codename(s). Separate with commas. Wildcard can be used\n'
+                         'eg: login*,*.add*,*.delete*'),
+        ('apply', False, 'Apply the result of the perm query.')
+
+    ]
+
+    def run(self):
+        from zengine.models import User, Permission
+        user = User(username=self.manager.args.username,
+                    superuser=bool(self.manager.args.super_user)
+                    )
+
+        if self.manager.args.perms:
+            perms = []
+            for prt in self.manager.args.perms.split(','):
+                perms.append("code:%s" % prt)
+            query = " OR ".join(perms)
+            permissions = list(Permission.objects.raw(query))
+            print("Query result:")
+            print("\n ~ %s - %s" % (perm.name, perm.code) for perm in permissions)
+
+            if self.manager.args.apply:
+                for perm in permissions:
+                    user.Permissions(permission=perm)
+                user.save()
+                print("Applied %s perms to the user" % len(permissions))
+        else:
+            print("Existing permissions of the user:")
+            print("\n ~ %s - %s" % (perm.name, perm.code) for perm in user.Permissions)
