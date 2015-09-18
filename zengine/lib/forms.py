da087338ba3c3dc1880d19d06bf36d34472b3d25
Evren Esat Özkan <evrenesat@zetaops.io>
2015-09-18 11:08:33 +0300

    various improvements and fixes

--- zengine/lib/forms.py
+++ zengine/lib/forms.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date
-from pyoko.field import DATE_FORMAT
+from pyoko.field import DATE_FORMAT, DATE_TIME_FORMAT
 
 from pyoko.form import Form
 
@@ -16,9 +16,14 @@ class JsonForm(Form):
             "model": {}
         }
         for itm in self._serialize():
-            if isinstance(itm['value'], (date, datetime)):
+            if isinstance(itm['value'], datetime):
+                itm['value'] = itm['value'].strftime(DATE_TIME_FORMAT)
+            elif isinstance(itm['value'], date):
                 itm['value'] = itm['value'].strftime(DATE_FORMAT)
+
             item_props = {'type': itm['type'], 'title': itm['title']}
+            if itm['type'] == 'model':
+                item_props['model_name'] = itm['model_name']
             if 'schema' in itm:
                 item_props['schema'] = itm['schema']
             result["schema"]["properties"][itm['name']] = item_props
--- zengine/views/crud.py
+++ zengine/views/crud.py
@@ -10,8 +10,10 @@ from falcon import HTTPNotFound
 
 from pyoko.model import Model, model_registry
 from zengine.lib.forms import JsonForm
+from zengine.log import getlogger
 from zengine.views.base import BaseView
 
+log = getlogger()
 
 class CrudView(BaseView):
     """
@@ -61,10 +63,12 @@ class CrudView(BaseView):
         self.output['object'] = self.form.serialize()['model']
         self.output['client_cmd'] = 'show_object'
 
-    def get_list_obj(self, mdl):
-        result = [mdl.key]
-        if mdl.Meta.list_fields:
-            for f in mdl.Meta.list_fields:
+    def get_list_obj(self, mdl, brief):
+        if brief:
+            return [mdl.key, unicode(mdl)]
+        else:
+            result = [mdl.key]
+            for f in self.object.Meta.list_fields:
                 field = getattr(mdl, f)
                 if callable(field):
                     result.append(field())
@@ -72,20 +76,19 @@ class CrudView(BaseView):
                     result.append(mdl._fields[f].clean_value(field))
                 else:
                     result.append(field)
-        else:
-            result.append(unicode(mdl))
-        return result
+            return result
 
     def list_view(self):
         # TODO: add pagination
         # TODO: investigate and if neccessary add sequrity/sanity checks for search params
+        brief = 'brief' in self.input
         query = self.object.objects.filter()
         if 'filters' in self.input:
             query = query.filter(**self.input['filters'])
         self.output['client_cmd'] = 'list_objects'
         self.output['nobjects'] = []
         self.output['objects'] = []
-        if self.object.Meta.list_fields:  # add list headers
+        if self.object.Meta.list_fields and not brief:  # add list headers
             list_headers = []
             for f in self.object.Meta.list_fields:
                 if callable(getattr(self.object, f)):
@@ -93,18 +96,21 @@ class CrudView(BaseView):
                 else:
                     list_headers.append(self.object._fields[f].title)
             self.output['nobjects'].append(list_headers)
-
+        make_it_brief = brief or not self.object.Meta.list_fields
         for obj in query:
             if ('deleted_obj' in self.current.task_data and self.current.task_data[
                 'deleted_obj'] == obj.key):
                 del self.current.task_data['deleted_obj']
                 continue
-            self.output['nobjects'].append(self.get_list_obj(obj))
+            self.output['nobjects'].append(self.get_list_obj(obj, make_it_brief))
             self.output['objects'].append({"data": obj.clean_field_values(), "key": obj.key})
         if 'added_obj' in self.current.task_data:
-            new_obj = self.object.objects.get(self.current.task_data['added_obj'])
-            self.output['nobjects'].insert(0, self.get_list_obj(new_obj))
-            self.output['objects'].insert(0, {"data": new_obj.clean_field_values(), "key": new_obj.key})
+            try:
+                    new_obj = self.object.objects.get(self.current.task_data['added_obj'])
+                    self.output['nobjects'].insert(0, self.get_list_obj(new_obj, make_it_brief))
+                    self.output['objects'].insert(0, {"data": new_obj.clean_field_values(), "key": new_obj.key})
+            except:
+                log.exception("ERROR while adding newly created object to object listing")
             del self.current.task_data['added_obj']
         self.output
 
