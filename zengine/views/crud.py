f78e3bf9aa43de79d15e81cff13f3bf58534037b
Evren Esat Özkan <sleytr@gmail.com>
2015-11-13 23:45:09 +0200

    added CRUDRegistry metaclass to ease the configuration through "Meta" configuration class  unified edit_view and add_view to "form_view"  added "meta" dict to output data to limit and configure behaviour of client  converted client_cmd in to list

--- zengine/views/crud.py
+++ zengine/views/crud.py
@@ -15,30 +15,41 @@ from pyoko.model import Model, model_registry
 from zengine.auth.permissions import NO_PERM_TASKS_TYPES
 from zengine.lib.forms import JsonForm
 from zengine.log import log
-from zengine.views.base import BaseView
+from zengine.views.base import BaseView, NEXT_CMD_SPLITTER
 
 
 # GENERIC_COMMANDS = ['edit', 'add', 'update', 'list', 'delete', 'do', 'show', 'save']
 
 
-# class CRUDRegistry(type):
-#     registry = {}
-#
-#     def __init__(mcs, name, bases, attrs):
-#         CRUDRegistry.registry[mcs.__name__] = mcs
-#
-#     @classmethod
-#     def _get_permission_names(cls):
-#         perms = []
-#         for kls_name, kls in cls.registry.items():
-#             for method_name in cls.__dict__.keys():
-#                 if method_name.endswith('_view'):
-#                     perms.append("%s.%s" % (kls_name, method_name))
-#         return perms
+class CRUDRegistry(type):
+    registry = {}
+    _meta = None
+
+    def __new__(mcs, name, bases, attrs):
+        if name == 'CrudView':
+            CRUDRegistry._meta = attrs['Meta']
+        else:
+            CRUDRegistry.registry[mcs.__name__] = mcs
+            if 'Meta' not in attrs:
+                attrs['Meta'] = type('Meta', (object,), CRUDRegistry._meta.__dict__)
+            else:
+                for k, v in CRUDRegistry._meta.__dict__.items():
+                    if k not in attrs['Meta'].__dict__:
+                        setattr(attrs['Meta'], k, v)
+        new_class = super(CRUDRegistry, mcs).__new__(mcs, name, bases, attrs)
+        return new_class
+
+    @classmethod
+    def get_permissions(cls):
+        perms = []
+        for kls_name, kls in cls.registry.items():
+            for method_name in cls.__dict__.keys():
+                if method_name.endswith('_view'):
+                    perms.append("%s.%s" % (kls_name, method_name))
+        return perms
 
 
-
-# @six.add_metaclass(CRUDRegistry)
+@six.add_metaclass(CRUDRegistry)
 class CrudView(BaseView):
     """
     A base class for "Create List Show Update Delete" type of views.
@@ -56,8 +67,11 @@ class CrudView(BaseView):
     class Meta:
         model = None
         init_view = 'list'
-        filter = {}
-        allow_client_filters = True
+        default_filter = {}
+        allow_filters = True
+        allow_edit = True
+        allow_add = True
+        objects_per_page = 20
         title = None
 
     class CrudForm(JsonForm):
@@ -76,9 +90,19 @@ class CrudView(BaseView):
         self.check_for_permission()
         current.log.info('Calling %s_view of %s' % ((self.cmd or 'list'),
                                                     self.object.__class__.__name__))
+        self.client_cmd = set()
+        self.output['meta'] = {
+            'allow_filters': self.Meta.allow_filters,
+            'allow_edit': self.Meta.allow_edit,
+            'allow_add': self.Meta.allow_add
+        }
         getattr(self, '%s_view' % self.cmd)()
         if self.next_cmd:
             self.current.task_data['cmd'] = self.next_cmd
+
+    def set_client_cmd(self, cmd):
+        self.client_cmd.add(cmd)
+        self.output['client_cmd'] = list(self.client_cmd)
 
     def check_for_permission(self):
         permission = "%s.%s" % (self.object.__class__.__name__, self.cmd)
@@ -115,19 +139,6 @@ class CrudView(BaseView):
         else:
             self.object = model_class(self.current)
 
-    # def list_models(self):
-    #     self.output["models"] = [(m.Meta.verbose_name_plural, m.__name__)
-    #                              for m in model_registry.get_base_models()]
-    #
-    #     self.output["app_models"] = [(app, [(m.Meta.verbose_name_plural, m.__name__)
-    #                                         for m in models])
-    #                                  for app, models in model_registry.get_models_by_apps()]
-
-    def show_view(self):
-        self.output['object'] = self.form.serialize()['model']
-        self.output['object']['key'] = self.object.key
-        self.output['client_cmd'] = 'show_object'
-
     def _get_list_obj(self, mdl):
         if self.brief:
             return [mdl.key, unicode(mdl) if six.PY2 else mdl]
@@ -156,9 +167,9 @@ class CrudView(BaseView):
             self.output['objects'].append('-1')
 
     def _process_list_filters(self, query):
-        if self.Meta.filter:
-            query = query.filter(**self.Meta.filter)
-        if self.Meta.allow_client_filters:
+        if self.Meta.default_filter:
+            query = query.filter(**self.Meta.default_filter)
+        if self.Meta.allow_filters:
             if self.current.request.params:
                 query = query.filter(**self.current.request.params)
             if 'filters' in self.input:
@@ -173,30 +184,22 @@ class CrudView(BaseView):
             return query.raw(search_string)
         return query
 
-    def list_view(self):
-        # TODO: add pagination
-        self.brief = 'brief' in self.input or not self.object.Meta.list_fields
-        query = self.object.objects.filter()
-        query = self._process_list_filters(query)
-        query = self._process_list_search(query)
-        self.output['client_cmd'] = 'list_objects'
-        self.output['objects'] = []
-        self._make_list_header()
-        for obj in query:
-            if self._just_deleted_object(obj):
-                continue
-            self.output['objects'].append(self._get_list_obj(obj))
-        self._just_created_object(self.output['objects'])
-
-    def _just_deleted_object(self, obj):
-        # compensate riak~solr sync delay
+    def _is_just_deleted_object(self, obj):
+        """
+        to compensate riak~solr sync delay, remove just deleted
+        object from from object list (if exists)
+        """
         if ('deleted_obj' in self.current.task_data and
                     self.current.task_data['deleted_obj'] == obj.key):
             del self.current.task_data['deleted_obj']
             return True
 
-    def _just_created_object(self, objects):
-        # compensate riak~solr sync delay
+    def _add_just_created_object(self, objects):
+        """
+        to compensate riak~solr sync delay, add just created
+        object to the object list
+        :param objects:
+        """
         if 'added_obj' in self.current.task_data:
             key = self.current.task_data['added_obj']
             if not any([o[0] == key for o in objects]):
@@ -204,13 +207,9 @@ class CrudView(BaseView):
                 self.output['objects'].insert(1, self._get_list_obj(obj))
                 del self.current.task_data['added_obj']
 
-    def edit_view(self):
+    def form_view(self):
         self.output['forms'] = self.form.serialize()
-        self.output['client_cmd'] = 'edit_object'
-
-    def add_view(self):
-        self.output['forms'] = self.form.serialize()
-        self.output['client_cmd'] = 'add_object'
+        self.set_client_cmd('form')
 
     def save_view(self):
         self.object = self.form.deserialize(self.current.input['form'])
@@ -218,7 +217,6 @@ class CrudView(BaseView):
         self.object.save()
         if self.next_cmd and obj_is_new:
             self.current.task_data['added_obj'] = self.object.key
-            # self.current.task_data['object_id'] = self.object.key
 
     def delete_view(self):
         # TODO: add confirmation dialog
@@ -227,3 +225,28 @@ class CrudView(BaseView):
         self.object.delete()
         del self.current.input['object_id']
         # del self.current.task_data['object_id']
+
+    def list_view(self):
+        # TODO: add pagination
+        self.set_client_cmd('list')
+        self.brief = 'brief' in self.input or not self.object.Meta.list_fields
+        query = self.object.objects.filter()
+        query = self._process_list_filters(query)
+        query = self._process_list_search(query)
+
+        self.output['objects'] = []
+        self._make_list_header()
+        for obj in query:
+            if self._is_just_deleted_object(obj):
+                continue
+            self.output['objects'].append(self._get_list_obj(obj))
+        self._add_just_created_object(self.output['objects'])
+
+    def show_view(self):
+        self.set_client_cmd('show')
+        self.output['object'] = self.form.serialize()['model']
+        self.output['object']['key'] = self.object.key
+
+    def list_form_view(self):
+        self.form_view()
+        self.list_view()
