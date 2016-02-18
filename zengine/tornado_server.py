d5f047cef4a5fd7a79d02a86c8f86913a79af79f
Evren Esat Özkan <evrenesat@zetaops.io>
2016-02-18 17:21:41 +0200

    rref #5134 ref GH-38

---
+++ zengine/tornado_server.py
@@ -0,0 +1,83 @@
+# -*-  coding: utf-8 -*-
+"""
+We created a Falcon based WSGI server.
+Integrated session support with beaker.
+Then route all requests to ZEngine.run() that runs SpiffWorkflow engine
+and invokes associated activity methods.
+
+Request and response objects for json data processing done at the middleware layer,
+thus, activity methods (which will be invoked from workflow engine)
+can simply read json data from current.input and write back to current.output
+
+"""
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+import json
+import traceback
+from falcon.http_error import HTTPError
+import falcon
+from beaker.middleware import SessionMiddleware
+from pyoko.lib.utils import get_object_from_path
+from zengine.log import log
+
+from zengine.config import settings
+from zengine.engine import ZEngine, Current
+
+# receivers should be imported at right time, right place
+# they will not registered if not placed in a central location
+# but they can cause "cannot import settings" errors if imported too early
+from zengine.receivers import *
+
+
+from tornado import websocket, web, ioloop
+import json
+
+from zengine.views.auth import Login
+
+cl = []
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+class LoginHandler(web.RequestHandler):
+
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+
+        current = Current()
+        Login(current)
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+app = web.Application([
+    # (r'/', IndexHandler),
+    (r'/ws', SocketHandler),
+    (r'/login/([^/]+)', LoginHandler),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
+
+
---
+++ zengine/wf_daemon.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+channel2 = connection.channel()
+
+channel.queue_declare(queue='hello')
+channel2.queue_declare(queue='ehlo')
+
+
+# print(" [x] Sent 'Hello World!'")
+# connection.close()
+
+def callback(ch, method, properties, body):
+    print(body)
+    time.sleep(1)
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+
+channel2.basic_consume(callback,
+                      queue='ehlo',
+                      no_ack=True)
+
+channel2.start_consuming()
+
