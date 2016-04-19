c543e3b827c89d2bc1889428cfd37b4f7578e778
Evren Esat Özkan <evrenesat@zetaops.io>
2016-04-19 17:15:32 +0300

    rref #5283 p10 #5283

--- zengine/views/__init__.py
+++ zengine/views/__init__.py
@@ -1,1 +1,16 @@
 __author__ = 'Evren Esat Ozkan'
+
+# def basic_view(name):
+#     def wrapper(func):
+#         from pyoko.conf import settings
+#         settings.VIEW_METHODS[name] = func
+#         return func
+#
+# class basic_view(object):
+#
+#     def __call__(self, cls):
+#         class Wrapped(cls):
+#             classattr = self.arg
+#             def new_method(self, value):
+#                 return value * 2
+#         return Wrapped
