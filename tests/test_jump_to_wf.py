818a8bf6b1ed1da583331e4d661c74a33bc2a7d6
Evren Esat Özkan <sleytr@gmail.com>
2016-06-09 17:35:41 +0300

    jump to wf initial tests rref #5268 #GH-47

---
+++ tests/test_jump_to_wf.py
@@ -0,0 +1,26 @@
+# -*-  coding: utf-8 -*-
+"""
+"""
+
+# Copyright (C) 2015 ZetaOps Inc.
+#
+# This file is licensed under the GNU General Public License v3
+# (GPLv3).  See LICENSE.txt for details.
+import pytest
+from pyoko.conf import settings
+from pyoko.db.adapter.db_riak import BlockSave
+from zengine.lib.exceptions import HTTPError
+from zengine.lib.test_utils import BaseTestCase
+from zengine.models import User
+from zengine.notifications.model import NotificationMessage
+from zengine.signals import lane_user_change
+
+
+class TestCase(BaseTestCase):
+    def test_jump_to_wf(self):
+        self.prepare_client('/jump_to_wf/', username='super_user')
+        resp = self.client.post()
+        # assert resp.json['msgbox']['title'] =='inner_title'
+        # resp = self.client.post()
+        # assert resp.json['msgbox']['title'] == 'otter_title'
+
