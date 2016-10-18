7d32d8c32b8fc0d31574fb63e86f4bdf4c3d6279
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-10-18 14:05:13 +0300

    ADD, new 2 models. rref #5407

--- tests/models.py
+++ tests/models.py
@@ -21,3 +21,25 @@ class Project(Model):
     @property
     def assigned_employees(self):
         return map(Project.Employee.get_user, self.Employee)
+
+
+class Teacher(Model):
+    class Meta:
+        search_fields = ['name', 'surname']
+
+    name = field.String('Alan')
+    surname = field.String("Turing")
+
+    def __unicode__(self):
+        return "%s %s" % (self.name, self.surname)
+
+
+class Exam(Model):
+    class Meta:
+        search_fields = ['teacher', 'exam_type', 'date']
+    teacher = Teacher()
+    exam_type = field.Integer(default=1)
+    date = field.DateTime()
+
+    def __unicode__(self):
+        return "%s %s" % (self.teacher.name, self.teacher.surname)
