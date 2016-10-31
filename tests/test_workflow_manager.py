d89d1b29ca19e49741b6d51e15ec23fcb676988d
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-10-31 16:48:42 +0200

    FIX, test removing data. rref #5407

--- tests/test_workflow_manager.py
+++ tests/test_workflow_manager.py
@@ -171,10 +171,6 @@ class TestCase(BaseTestCase):
 
     def test_object_and_query_task_manager(self):
 
-        Task.objects.filter()._clear()
-        WFInstance.objects.filter()._clear()
-        TaskInvitation.objects.filter()._clear()
-
         # create test task manager object with unit, abstract_role, object_type and object_query_code
         task = Task()
         task.wf = BPMNWorkflow.objects.get(name='workflow_management')
@@ -183,21 +179,27 @@ class TestCase(BaseTestCase):
         task.abstract_role = AbstractRole.objects.get(name='Test AbstractRole')
         task.object_type = 'Program'   # Program model
         task.object_query_code = {'role': 'role'}
-        task.start_date = datetime.strptime('10.10.2016', '%d.%m.%Y')
-        task.finish_date = datetime.strptime('12.10.2016', '%d.%m.%Y')
+        task.start_date = datetime.strptime('05.05.2016', '%d.%m.%Y')
+        task.finish_date = datetime.strptime('06.05.2016', '%d.%m.%Y')
         task.run = True
         task.save()
 
         time.sleep(1)
 
         # expected task manager objects numbers
-        assert len(Task.objects.filter()) == 1
-        assert len(WFInstance.objects.filter()) == 5
-        assert len(TaskInvitation.objects.filter()) == 5
 
-        Task.objects.filter().delete()
-        WFInstance.objects.filter().delete()
-        TaskInvitation.objects.filter().delete()
+        tsk = Task.objects.filter(start_date=datetime(2016, 5, 5, 0, 0))
+
+        taskinv = TaskInvitation.objects.filter(start_date=datetime(2016, 5, 5, 0, 0))
+
+        assert len(tsk) == 1
+        wfi = WFInstance.objects.filter(task=tsk[0])
+        assert len(wfi) == 5
+        assert len(taskinv) == 5
+
+        tsk.delete()
+        wfi.delete()
+        taskinv.delete()
 
     def test_abstractrole_and_unit_task_manager(self):
 
@@ -207,21 +209,25 @@ class TestCase(BaseTestCase):
         task.name = task.wf.title
         task.unit = Unit.objects.get(name='Test Unit 2')
         task.abstract_role = AbstractRole.objects.get(name='Test AbstractRole 2')
-        task.start_date = datetime.strptime('10.10.2016', '%d.%m.%Y')
-        task.finish_date = datetime.strptime('12.10.2016', '%d.%m.%Y')
+        task.start_date = datetime.strptime('06.06.2016', '%d.%m.%Y')
+        task.finish_date = datetime.strptime('07.06.2016', '%d.%m.%Y')
         task.run = True
         task.save()
 
         time.sleep(1)
 
         # expected task manager objects numbers
-        assert len(Task.objects.filter()) == 1
-        assert len(WFInstance.objects.filter()) == 1
-        assert len(TaskInvitation.objects.filter()) == 1
+        tsk = Task.objects.filter(start_date=datetime(2016, 6, 6, 0, 0))
+        role = Role.objects.get(unit=task.unit, abstract_role=task.abstract_role)
+        assert len(tsk) == 1
+        wfi = WFInstance.objects.filter(task=tsk[0])
+        assert len(wfi) == 1
+        taskinv = TaskInvitation.objects.filter(start_date=datetime(2016, 6, 6, 0, 0))
+        assert len(taskinv) == 1
 
-        Task.objects.filter().delete()
-        WFInstance.objects.filter().delete()
-        TaskInvitation.objects.filter().delete()
+        tsk.delete()
+        wfi.delete()
+        taskinv.delete()
 
     def test_wf_and_role_getter_task_manager(self):
 
@@ -230,21 +236,24 @@ class TestCase(BaseTestCase):
         task.wf = BPMNWorkflow.objects.get(name='workflow_management')
         task.name = task.wf.title
         task.get_roles_from = 'get_test_role'
-        task.start_date = datetime.strptime('10.10.2016', '%d.%m.%Y')
-        task.finish_date = datetime.strptime('12.10.2016', '%d.%m.%Y')
+        task.start_date = datetime.strptime('07.07.2016', '%d.%m.%Y')
+        task.finish_date = datetime.strptime('08.07.2016', '%d.%m.%Y')
         task.run = True
         task.save()
 
         time.sleep(1)
 
         # expected task manager objects numbers
-        assert len(Task.objects.filter()) == 1
-        assert len(WFInstance.objects.filter()) == 1
-        assert len(TaskInvitation.objects.filter()) == 2
+        tsk = Task.objects.filter(start_date=datetime(2016, 7, 7, 0, 0))
+        assert len(tsk) == 1
+        wfi = WFInstance.objects.filter(task=task)
+        assert len(wfi) == 1
+        taskinv = TaskInvitation.objects.filter(start_date=datetime(2016, 7, 7, 0, 0))
+        assert len(taskinv) == 2
 
-        Task.objects.filter().delete()
-        WFInstance.objects.filter().delete()
-        TaskInvitation.objects.filter().delete()
+        tsk.delete()
+        wfi.delete()
+        taskinv.delete()
 
     def test_role_getter_object_and_query_task_manager(self):
         # create test task manager object with wf, object_type, object_query_code and role_getter object
@@ -254,17 +263,21 @@ class TestCase(BaseTestCase):
         task.get_roles_from = 'get_test_role'
         task.object_type = 'Program'   # Program model
         task.object_query_code = {'type': 1} # Program.objects.filter(type = 1)
-        task.start_date = datetime.strptime('10.10.2016', '%d.%m.%Y')
-        task.finish_date = datetime.strptime('12.10.2016', '%d.%m.%Y')
+        task.start_date = datetime.strptime('08.08.2016', '%d.%m.%Y')
+        task.finish_date = datetime.strptime('09.08.2016', '%d.%m.%Y')
         task.run = True
         task.save()
 
         time.sleep(1)
 
-        assert len(Task.objects.filter()) == 1
-        assert len(WFInstance.objects.filter()) == 3
-        assert len(TaskInvitation.objects.filter()) == 6
+        # expected task manager objects numbers
+        tsk = Task.objects.filter(start_date=datetime(2016, 8, 8, 0, 0))
+        assert len(tsk) == 1
+        wfi = WFInstance.objects.filter(task=task)
+        assert len(wfi) == 3
+        taskinv = TaskInvitation.objects.filter(start_date=datetime(2016, 8, 8, 0, 0))
+        assert len(taskinv) == 6
 
-        Task.objects.filter()._clear()
-        WFInstance.objects.filter()._clear()
-        TaskInvitation.objects.filter()._clear()
+        tsk.delete()
+        wfi.delete()
+        taskinv.delete()
