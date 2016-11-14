a949a4a32febd4acb481e1db26415e8f65d8cc18
Tolga Ozturk <oztrk.tolga@gmail.com>
2016-11-14 16:43:41 +0300

    ADD:suspend manager action and refortor other actions. rref #5517

--- zengine/views/task_manager_actions.py
+++ zengine/views/task_manager_actions.py
@@ -16,8 +16,14 @@ RoleModel = get_object_from_path(settings.ROLE_MODEL)
 
 class TaskManagerActionsView(CrudView):
 
+    def message(self, title, msg, typ='info'):
+        self.output['msgbox'] = {
+            'type': typ, "title": title,
+            "msg": msg
+        }
+
     # - Assign Yourself -
-    def control_task_invitation(self):
+    def assign_yourself(self):
         task_invitation_key = self.input['task_key']
         task_invitation = TaskInvitation.objects.get(task_invitation_key)
         wfi = task_invitation.instance
@@ -26,26 +32,16 @@ class TaskManagerActionsView(CrudView):
             wfi.current_actor = self.current.role
             wfi.save()
             [inv.delete() for inv in TaskInvitation.objects.filter(instance=wfi) if not inv == task_invitation]
-            self.current.task_data['actions'] = True
+            title = "Successful",
+            msg = ""
         else:
-            self.current.task_data['actions'] = False
-            self.current.task_data['current_actor_key'] = wfi.current_actor.key
+            title = "Unsuccessful",
+            msg = ""
 
-    def show_message(self):
-        if self.current.task_data['actions']:
-            self.output['msgbox'] = {
-                'type': 'info', "title": "Successful",
-                "msg": ""
-            }
-        else:
-            self.output['msgbox'] = {
-                'type': 'info', "title": "Unsuccessful",
-                "msg": ""
-            }
+        self.message(title=title, msg=msg)
     # - Assign Yourself -
 
     # - Assign to same abstract role and unit -
-
     def select_role(self):
         roles = RoleModel.objects.filter(abstract_role=self.current.role.abstract_role, unit=self.current.role.unit)
         self.current.task_data['task_inv_key'] = self.input['task_key']
@@ -56,7 +52,9 @@ class TaskManagerActionsView(CrudView):
             _form.send_button = fields.Button("Send")
             self.form_out(_form)
         else:
-            pass
+            title = "Unsuccessful",
+            msg = ""
+            self.message(title=title, msg=msg)
 
     def send_workflow(self):
         task_invitation = TaskInvitation.objects.get(self.current.task_data['task_inv_key'])
@@ -65,26 +63,24 @@ class TaskManagerActionsView(CrudView):
             wfi.current_actor = self.input['form']['select_role']
             wfi.save()
             [inv.delete() for inv in TaskInvitation.objects.filter(instance=wfi) if not inv == task_invitation]
-            self.output['msgbox'] = {
-                'type': 'info', "title": "Successful",
-                "msg": ""
-            }
+            title = "Successful",
+            msg = ""
         else:
-            self.output['msgbox'] = {
-                'type': 'info', "title": "Unsuccessful",
-                "msg": ""
-            }
+            title = "Unsuccessful",
+            msg = ""
+        self.message(title=title, msg=msg)
+
     # - Assign to same abstract role and unit -
 
-    # - Suspend workflow -
-    def select_suspend_date(self):
+    # - Postponed workflow -
+    def select_postponed_date(self):
         _form = forms.JsonForm(title="Suspend Workflow")
         _form.start_date = fields.DateTime("Start Date")
         _form.finish_date = fields.DateTime("Finish Date")
         _form.save_button = fields.Button("Save")
         self.form_out(_form)
 
-    def suspend(self):
+    def save_date(self):
         task_invitation_key = self.input['task_key']
         task_invitation = TaskInvitation.objects.get(task_invitation_key)
         wfi = task_invitation.instance
@@ -97,8 +93,25 @@ class TaskManagerActionsView(CrudView):
         wfi.finish_date = self.input['form']['finish_date']
         wfi.save()
 
-        self.output['msgbox'] = {
-            'type': 'info', "title": "Successful",
-            "msg": ""
-        }
+        title = "Successful",
+        msg = ""
+        self.message(title=title, msg=msg)
+    # - Postponed workflow -
+
+    # - Suspend workflow -
+    def suspend(self):
+        task_invitation_key = self.input['task_key']
+        task_invitation = TaskInvitation.objects.get(task_invitation_key)
+        wfi = task_invitation.instance
+
+        if wfi.current_actor.exist:
+            wfi.current_actor = None
+            wfi.save()
+            title = "Successful",
+            msg = ""
+        else:
+            title = "Unsuccessful",
+            msg = ""
+
+        self.message(title=title, msg=msg)
     # - Suspend workflow -
