1cdc1af10e82e4262126f087768acc01ae42524a
Anil Can Aydin <anlcnydn@gmail.com>
2017-05-03 12:25:53 +0300

    ADD, Returned to the previous version. rref #5657

--- zengine/forms/json_form.py
+++ zengine/forms/json_form.py
@@ -247,10 +247,7 @@ class JsonForm(ModelForm):
                 item_props['widget'] = itm['kwargs'].pop('widget')
 
             if form_data:
-                if itm['type'] == 'date':
-                    value_to_serialize = datetime.strptime(
-                        form_data[itm['name']], itm['format']).date()
-                elif itm['type'] == 'datetime':
+                if itm['type'] == 'date' or itm['type'] == 'datetime':
                     value_to_serialize = datetime.strptime(
                         form_data[itm['name']], itm['format'])
                 else:
