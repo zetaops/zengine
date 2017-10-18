24abb36b1ed77294f780c15d0ab37cdd5d27c74f
Ali Riza Keles <ali.r.keles@gmail.com>
2017-10-18 09:32:06 +0300

    version bump 0.8.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='zengine',
-    version='0.8.0',
+    version='0.8.1',
     url='https://github.com/zetaops/zengine',
     license='GPL v3',
     packages=find_packages(exclude=['tests', 'tests.*']),
